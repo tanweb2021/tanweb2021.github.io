6b48bae4660f0751f0f89cac2b4a77153aad389e
tanweb2021 <90164454+tanweb2021@users.noreply.github.com>
2021-09-05 22:47:58 -0500

    Update 2

---
+++ _site/js/lunrsearchengine.js
@@ -0,0 +1,114 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/about",
+    "title": "Mediumish Template for Jekyll",
+    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/",
+    "title": "Home",
+    "body": "      Featured:                                                                                                                                                                                                                       தமிழ் அறிஞர்கள் நாள்                              :               மகாகவி பாரதியாரின் நூற்றாண்டு நினைவு நாளையொட்டி, சிகாகோ தமிழ்ச் சங்கம் அக்டோபர் மாதம் 2 ஆம் நாளை “தமிழறிஞர்கள் நாள்” எனப் பெருமையுடன் அறிவிக்கிறது. நிகழும் ஆண்டில் விழா காணும் அறிஞர்களைப் போற்றும் நாளாக. . . :                                                                                                                                                                       Chicago Tamil Sangam                                31 Aug 2021                                                                                                                            All Stories:                                                                                                     மகாகவி பாரதியார்              :       மகாகவி பாரதியார் அவர்கள், 1882 ஆம் ஆண்டு தமிழ்நாட்டின் திருநெல்வேலி மாவட்டத்திலுள்ள எட்டயபுரத்தில் பிறந்தார். அவருக்கு பெற்றோர் இட்ட பெயர் சுப்பிரமணியன். அவருடைய 5 வயதில் அவருடைய தாயார் காலமானார். இவர் இளம் வயதிலேயே தமிழில் புலமைப்பெற்றுத் திகழ்ந்தார். :                                                                               Chicago Tamil Sangam                01 Sep 2021                                                                                                                                     உவமைக் கவிஞர் சுரதா              :       ‘உவமைக் கவிஞர்’ சுரதா தஞ்சை மாவட்டம் பழையனூரில் (சிக்கல்) 1921-ல் பிறந்தார். இயற்பெயர் ராஜகோபாலன். பள்ளி இறுதி வகுப்பு வரை பயின்றார். சீர்காழி அருணாசல தேசிகரிடம் தமிழ் இலக்கணங்கள் கற்றார். பாவேந்தர் பாரதிதாசன் மீது மிகுந்த பற்று கொண்டவர். :                                                                               Chicago Tamil Sangam                31 Aug 2021                                                                                                                                     பதிவு / Registration              :       தமிழ் அறிஞர் நாள் விழாவில் பங்கேற்க கவிஞர்கள் மற்றும் தமிழ் ஆர்வலர்களை அன்புடன் அழைக்கின்றோம். :                                                                               Chicago Tamil Sangam                30 Aug 2021                                            "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 5,
+    "url": "http://localhost:4000/Bharathiar/",
+    "title": "மகாகவி பாரதியார்",
+    "body": "2021/09/01 - மகாகவி பாரதியார் அவர்கள், 1882 ஆம் ஆண்டு தமிழ்நாட்டின் திருநெல்வேலி மாவட்டத்திலுள்ள எட்டயபுரத்தில் பிறந்தார். அவருக்கு பெற்றோர் இட்ட பெயர் சுப்பிரமணியன். அவருடைய 5 வயதில் அவருடைய தாயார் காலமானார். இவர் இளம் வயதிலேயே தமிழில் புலமைப்பெற்றுத் திகழ்ந்தார். இளமைப் பருவம் சிறு வயதிலேயே பாரதியாருக்கு தமிழ் மொழி மீது சிறந்த பற்றும், புலமையும் இருந்தது. ஏழு வயதில் பள்ளியில் படித்துவரும்பொழுது கவிதைகள் எழுதத் தொடங்கினார். தன்னுடைய பதினொரு வயதில் கவிபாடும் ஆற்றலை வெளிப்படுத்தினார், இவருடைய கவிப்புலமையை பாராட்டிய எட்டயபுர மன்னர், இவருக்கு “பாரதி” என்ற பட்டத்தை வழங்கினார். அன்று முதல் இவர் “சுப்பிரமணிய பாரதியார்” என அழைக்கப்பெற்றார்.   . பாரதியாரின் திருமண வாழ்க்கை பாரதியார் அவர்கள், பள்ளியில் படித்துகொண்டிருக்கும் பொழுதே 1897 ஆம் ஆண்டு செல்லம்மா என்பவரைத் திருமணம் செய்து கொண்டார். தனது தந்தையின் இறப்புக்குப் பிறகு பாரதியார் வறுமை நிலையினை அடைந்தார். சிறிது காலம் காசிக்கு சென்று தங்கியிருந்தார். பிறகு எட்டையபுர மன்னரின் அழைப்பை ஏற்று அரசவை கவிஞராக பணியாற்றினார். பாரதியாரின் இலக்கிய பணி ‘மீசை கவிஞன்’ என்றும் ‘முண்டாசு கவிஞன்’ என்றும் தமிழ் இலக்கிய உலகம் போற்றும் பாரதியார், தாய் மொழியாம் தமிழ் மொழியின் மீது மிகுந்த பற்றுடையவராக திகழ்ந்தார். இவர் சமஸ்கிருதம், வங்காளம், இந்தி, ஆங்கிலம் போன்ற பிறமொழிகளிலும் தனி புலமைப்பெற்று விளங்கினார். 1912 ஆம் ஆண்டு கீதையை தமிழில் மொழிப்பெயர்த்தார். ‘கண்ணன்பாட்டு’, ‘குயில்பாட்டு’, ‘பாஞ்சாலி சபதம்’,’ புதிய ஆத்திச்சூடி’ போன்ற புகழ் பெற்ற காவியங்கள் பாரதியரால் எழுதப் பெற்றன. விடுதலைப் போராட்டத்தில் பாரதியின் பங்கு சுதந்திரப் போரில், பாரதியின் பாடல்கள் உணர்ச்சி வெள்ளமாய், காட்டுத்தீயாய், சுதந்திரக் கனலாய் தமிழ்நாட்டை வீருகொள்ளச் செய்தது. பாரதியார் “இந்திய பத்திரிக்கையின்” மூலம் மக்களிடையே விடுதலை உணர்வை தூண்டும் வகையில் பல எழுச்சியூட்டும் கட்டுரைகளை எழுதினார். பாரதியின் எழுச்சிக்கு, தமிழ்நாட்டில் பலத்த ஆதரவு பெருகுவதைக் கண்ட பிரிட்டிஷ் ஆட்சி “இந்தியா பத்திரிக்கைக்கு” தடை விதித்து அவரை கைது செய்து சிறையிலும் அடைத்தது. அதுமட்டுமல்லாமல், விடுதலைப் போராட்டக் காலத்தில் தேசிய உணர்வுள்ள பல்வேறு கவிதைகளைப் படைத்து மக்களை ஒருங்கிணைத்த காரணத்தால், பாரதி “தேசிய கவியாக” அனைவராலும் போற்றப்பட்டார். இவர் சுதேசிமித்திரனில் உதவி ஆசிரியராக, நவம்பர் 1904 முதல் ஆகஸ்ட் 1906 வரை பணியாற்றினார். “ஆடுவோமே பள்ளு பாடுவோமே ஆனந்த சுதந்திரம் அடைந்துவிட்டோம்” என்று சுதந்திரம் அடைவதற்கு முன்பே தன்னுடைய சுதந்திர தாகத்தை தன் பாட்டின் மூலம் வெளிபடுத்தியவர், மகாகவி சுப்பிரமணிய பாரதியார். இறப்பு 1921 ஆம் ஆண்டு ஜூலை மாதம் திருவல்லிகேணியில் உள்ள பார்த்தசாரதி கோவிலுக்கு சென்றபோது, எதிர்பாராவிதமாக அந்த கோவில் யானையால் தூக்கி எறியப்பட்டதால் பலத்த காயமுற்று மிகவும் நோய்வாய்ப்பட்டார். பிறகு, 1921 செப்டம்பர் 11ம் தேதி, தனது 39 ஆவது வயதில் இவ்வுலக வாழ்விலிருந்து விடுதலைப் பெற்றார். "
+    }, {
+    "id": 6,
+    "url": "http://localhost:4000/Suratha/",
+    "title": "உவமைக் கவிஞர் சுரதா",
+    "body": "2021/08/31 - ‘உவமைக் கவிஞர்’ சுரதா தஞ்சை மாவட்டம் பழையனூரில் (சிக்கல்) 1921-ல் பிறந்தார். இயற்பெயர் ராஜகோபாலன். பள்ளி இறுதி வகுப்பு வரை பயின்றார். சீர்காழி அருணாசல தேசிகரிடம் தமிழ் இலக்கணங்கள் கற்றார். பாவேந்தர் பாரதிதாசன் மீது மிகுந்த பற்று கொண்டவர். பாவேந்தரின் இயற்பெயரான சுப்புரத்தினம் என்பதை அடிப்படையாகக் கொண்டு ‘சுப்புரத்தினதாசன்’ என்று தன் பெயரை மாற்றிக்கொண்டார். இதன் சுருக்கமான ‘சுரதா’ என்ற பெயரில் இலக்கியப் படைப்பாளியாக பரிணமித்தார். செய்யுள் மரபு மாறாமல் உவமைகளுடன் கவிதை படைப்பதில் வல்லவர் என்பதால் ‘உவமைக் கவிஞர்’ என போற்றப்பட்டார். பாரதி தாசனை 1941 ஜனவரியில் சந்தித்தார். அவருடன் சில காலம் தங்கியிருந்து, அவரது கவிதைகளைப் படியெடுத்தல், அச்சுப் பணி, நூல் வெளியீடு போன்றவற்றில் உறுதுணையாக இருந்தார். ‘மங்கையர்க்கரசி’ திரைப்படத்துக்கு 1944-ல் வசனம் எழுதினார். ‘அமுதும் தேனும் எதற்கு’, ‘ஆடி அடங்கும் வாழ்க்கையடா’ என்பது போன்ற சாகாவரம் பெற்ற பாடல்கள் உட்பட 100-க்கும் மேற்பட்ட திரைப்பாடல்களை எழுதியுள்ளார். புதுக்கோட்டையில் இருந்து வெளிவந்த ‘தலைவன்’ இதழின் துணை ஆசிரியராகப் பணியாற்றினார். அப்போது பல சிறுகதைகள் எழுதினார். ‘சிவாஜி’ இதழில் கவிதைகள் எழுதினார். இது பின்னர் கவிதைத் தொகுப்பாக வெளியிடப்பட்டது. முதல் நூலான ‘சாவின் முத்தம்’ 1946-ல் வெளிவந்தது. 1955-ல் ‘காவியம்’ என்ற வார இதழைத் தொடங்கினார். ‘இலக்கியம்’, ‘ஊர்வலம்’, ‘விண்மீன்’, ‘சுரதா’ என பல கவிதை இதழ்களை வெளியிட்டார். நடிகைகளின் வாழ்க்கையைப் படம்பிடித்துக்காட்டும் வகையில் ஆனந்த விகடன் இதழில் 1971-ல் வெளிவந்த இவரது கவிதைகள் பெரும் வரவேற்பைப் பெற்றன. உலகத் தமிழ்க் கவிஞர் பேரவையைத் தொடங்கியவர். 3 ஆயிரத்துக்கும் மேற்பட்ட கவியரங்குகளை தலைமையேற்று நடத்தியவர். வீட்டுக்கு வீடு கவியரங்கம், முழுநிலாக் கவியரங்கம், படகுக் கவியரங்கம், ஆற்றுக் கவியரங்கம், கப்பல் கவியரங்கம் என புதுமையான கவியரங்குகளை நடத்தி இளைஞர்களிடம் கவிதை ஆர்வத்தை ஏற்படுத்தினார். யாரையும் பின்பற்றி எழுதுவதில் உடன்பாடு இல்லாதவர். இவரது கவிதைகள் பள்ளி, கல்லூரி, பல்கலைக்கழக பாடநூல்களில் இடம்பெற்றுள்ளன. பல மொழிகளில் மொழிபெயர்க்கப்பட்டுள்ளன. மருதுபாண்டியர் உள்ளிட்ட வரலாற்று நாயகர்கள் குறித்த அரிய தகவல்களை புத்தக வடிவில் ஆவணப்படுத்தியுள்ளார். பல நூல்களாக இருந்த பாரதிதாசன் கவிதைகள் இவரது முயற்சியால் ஒரே தொகுப்பாக வெளியிடப்பட்டது. தமிழக அரசின் பாரதிதாசன் விருது, கலைமாமணி விருது, தஞ்சை தமிழ்ப் பல்கலைக்கழகத்தின் ராஜராஜன் விருது, மகாகவி குமரன் ஆசான் விருது உள்ளிட்ட பல விருதுகள், பரிசுகள் பெற்றவர். மலேசியாவில் 1987-ல் நடந்த உலகத் தமிழ் மாநாட்டில் சிறப்பு அழைப்பாளராகப் பங்கேற்றார். இவரது தமிழ்த் தொண்டை கவுரவித்து சென்னையில் இவருக்கு சிலை நிறுவப்பட்டுள்ளது. முதன்முதலில் கவிதையிலேயே வார இதழ் நடத்திய பெருமைக்கு உரியவர். கவிதை படைப்பதை உயிர்மூச்சாகக் கொண்டு வாழ்ந்த ‘உவமைக் கவிஞர்’ சுரதா 85-வது வயதில் (2006) மறைந்தார். Buy me a coffee Documentation "
+    }, {
+    "id": 7,
+    "url": "http://localhost:4000/Intro/",
+    "title": "தமிழ் அறிஞர்கள் நாள்",
+    "body": "2021/08/31 - மகாகவி பாரதியாரின் நூற்றாண்டு நினைவு நாளையொட்டி, சிகாகோ தமிழ்ச் சங்கம் அக்டோபர் மாதம் 2 ஆம் நாளை “தமிழறிஞர்கள் நாள்” எனப் பெருமையுடன் அறிவிக்கிறது. நிகழும் ஆண்டில் விழா காணும் அறிஞர்களைப் போற்றும் நாளாக நாம் இணைந்து கொண்டாடுவோம். மேலும் விவரங்கள் விரைவில்! "
+    }, {
+    "id": 8,
+    "url": "http://localhost:4000/registration/",
+    "title": "பதிவு / Registration",
+    "body": "2021/08/30 - தமிழ் அறிஞர் நாள் விழாவில் பங்கேற்க கவிஞர்கள் மற்றும் தமிழ் ஆர்வலர்களை அன்புடன் அழைக்கின்றோம். "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
